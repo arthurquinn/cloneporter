e943a2afb0002277b06f14b7f59af3e6fdcaf45c
Arthur Toriello <arthur@aptoriello.com>
2024-09-05 07:00:21 -0400

    laser has been un scuffed

--- Assets/Scripts/Enemies/BurnoutController.cs
+++ Assets/Scripts/Enemies/BurnoutController.cs
@@ -1,8 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BurnoutController : MonoBehaviour
 {
+    [Header("Laser Stats")]
+    [SerializeField] private Transform _laserOriginPosition;
+    [SerializeField] private float _maxLaserDistance;
+    [SerializeField] private LayerMask _laserCollisionMask;
 
+    [Header("Events")]
+    [SerializeField] private UnityEvent<Vector2[]> _onLaserPositionsChanged;
+
+    private const int MAX_LASER_POSITIONS = 16;
+    private Vector2[] _laserPositions;
+
+    private void Awake()
+    {
+        _laserPositions = new Vector2[MAX_LASER_POSITIONS];
+    }
+
+    // TODO: Lots of room to optimize here
+    private void FixedUpdate()
+    {
+        ClearLaserPositions();
+        SetLaserPositions();
+        
+        // Ideally we will check to see if any positions changed, and then trigger the line update
+        //   Until then just always call this event (bad)
+        if (_onLaserPositionsChanged != null)
+        {
+            _onLaserPositionsChanged.Invoke(_laserPositions);
+        }
+    }
+
+    private void ClearLaserPositions()
+    {
+        for (int i = 0; i < _laserPositions.Length; i++)
+        {
+            _laserPositions[i] = Vector2.zero;
+        }
+    }
+
+    private void SetLaserPositions()
+    {
+        _laserPositions[0] = _laserOriginPosition.position;
+        RaycastHit2D hit = Physics2D.Raycast(_laserOriginPosition.position, Vector2.right, _maxLaserDistance, _laserCollisionMask);
+        if (hit.collider != null)
+        {
+            _laserPositions[1] = hit.point;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(_laserOriginPosition.position, Vector2.right);
+    }
 }
--- Assets/Scripts/Enemies/LaserController.cs
+++ Assets/Scripts/Enemies/LaserController.cs
@@ -8,61 +8,22 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class LaserController : MonoBehaviour
 {
-    [Header("Stats")]
-    [SerializeField] private float _laserDistance;
-    [SerializeField] private Vector2 _laserStartDirection;
-    [SerializeField] private LayerMask _laserInteractionLayers;
-
     private LineRenderer _lineRenderer;
-
-    // TODO: Don't use a List -- its so bad but I'm focusing on
-    //   shader for now
-    private List<Vector2> _laserPositions;
 
     private void Start()
     {
         _lineRenderer = GetComponent<LineRenderer>();
-        _laserPositions = new List<Vector2>();
     }
 
-    // TODO: Potential optimization: Only udpate laser coordinates
-    //   when position of tracked world objects change
-    private void Update()
+    public void SetLaser(Vector2[] positions)
     {
-        DrawLaser();
-    }
-
-    private void FixedUpdate()
-    {
-        SetLaserPositions();
-    }
-
-    private void DrawLaser()
-    {
-        int i = 0;
-        _lineRenderer.positionCount = _laserPositions.Count;
-        foreach (Vector2 position in _laserPositions)
+        for (int i = 0; i < positions.Length; i++)
         {
-            _lineRenderer.SetPosition(i++, position);
+            if (positions[i] == Vector2.zero)
+            {
+                break;
+            }
+            _lineRenderer.SetPosition(i, positions[i]);
         }
-    }
-
-    private void SetLaserPositions()
-    {
-        // Clear old list
-        _laserPositions.Clear();
-
-        // First position is always the origin
-        _laserPositions.Add(transform.position);
-
-        // Add collision
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, _laserStartDirection, _laserDistance, _laserInteractionLayers);
-        if (hit.collider != null)
-        {
-            _laserPositions.Add(hit.point);
-        }
-
-        _laserPositions.Add(new Vector2(15, 8));
-        _laserPositions.Add(new Vector2(-15, 8));
     }
 }
