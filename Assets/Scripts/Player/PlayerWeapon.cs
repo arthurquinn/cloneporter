8054d8af1174ff26e8185b129ae44e8aff4c6e4e
Arthur Toriello <arthur@aptoriello.com>
2024-09-09 04:51:00 -0400

    player can aim gun

--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -9,12 +9,11 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(LineRenderer))]
 public class PlayerWeapon : MonoBehaviour
 {
-    [Header("Player")]
-    [SerializeField] private Transform _playerTransform;
-
     [Header("Events")]
     [SerializeField] private UnityEvent<Vector2, Vector2> _onPurplePortalFire;
     [SerializeField] private UnityEvent<Vector2, Vector2> _onTealPortalFire;
+    [SerializeField] private UnityEvent<Vector2> _onPlayerAimPosition;
+    [SerializeField] private UnityEvent _onPlayerStopAim;
 
     [Header("Hit Detection")]
     [SerializeField] private float _raycastLength;
@@ -25,12 +24,14 @@ public class PlayerWeapon : MonoBehaviour
 
     [Header("Aim Target")]
     [SerializeField] private Transform _rightArmEffector;
+    [SerializeField] private Transform _initialAimOrigin;
 
     private PlayerInputActions _inputs;
     private LineRenderer _lineRenderer;
 
     private bool _isAiming;
     private Vector2 _aimDirection;
+    private Vector2 _aimOrigin;
 
     private void Awake()
     {
@@ -40,6 +41,8 @@ public class PlayerWeapon : MonoBehaviour
     private void Start()
     {
         _lineRenderer = GetComponent<LineRenderer>();
+
+        _aimOrigin = _initialAimOrigin.position;
     }
 
     private void OnEnable()
@@ -72,8 +75,6 @@ public class PlayerWeapon : MonoBehaviour
 
     private void Update()
     {
-        transform.position = _playerTransform.position;
-
         if (_isAiming)
         {
             DrawAimLine();
@@ -88,20 +89,23 @@ public class PlayerWeapon : MonoBehaviour
     {
         // Get normalized vector from weapon to mouse world position
         Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(_inputs.Player.Look.ReadValue<Vector2>());
-        _aimDirection = (mouseWorldPosition - (Vector2)transform.position).normalized;
+        _aimDirection = (mouseWorldPosition - _aimOrigin).normalized;
 
         // Raycast to find nearest collider
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, _aimDirection, _raycastLength, _targetLayer);
+        RaycastHit2D hit = Physics2D.Raycast(_aimOrigin, _aimDirection, _raycastLength, _targetLayer);
         Debug.Log(hit);
         if (hit.collider != null)
         {
             Debug.Log(hit.collider);
 
             // Draw line to nearest collider in mouse direction
-            Vector2 aimPos = (Vector2)transform.position + (_aimDirection * hit.distance);
+            Vector2 aimPos = _aimOrigin + (_aimDirection * hit.distance);
             _lineRenderer.enabled = true;
-            _lineRenderer.SetPosition(0, transform.position);
+            _lineRenderer.SetPosition(0, _aimOrigin);
             _lineRenderer.SetPosition(1, aimPos);
+
+            // Invoke our on aim event
+            _onPlayerAimPosition.Invoke(aimPos);
         }
         else
         {
@@ -125,28 +129,41 @@ public class PlayerWeapon : MonoBehaviour
     {
         _isAiming = false;
         TryShootPortal(_onPurplePortalFire);
+
+        _onPlayerStopAim.Invoke();
     }
 
     private void OnFireRightCanceled(InputAction.CallbackContext context)
     {
         _isAiming = false;
         TryShootPortal(_onTealPortalFire);
+
+        _onPlayerStopAim.Invoke();
     }
 
     private void TryShootPortal(UnityEvent<Vector2, Vector2> shootEvent)
     {
         // Check if we are aiming at a portal tile
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, _aimDirection, _raycastLength, _targetLayer);
+        RaycastHit2D hit = Physics2D.Raycast(_aimOrigin, _aimDirection, _raycastLength, _targetLayer);
         if (hit.collider != null && hit.collider.CompareTag("PortalTiles"))
         {
             // TODO: I really want something better than this
-            Vector2 adjustedHitPoint = hit.point + ((hit.point - (Vector2)transform.position).normalized) * _hitDetectionMultiplier;
+            Vector2 adjustedHitPoint = hit.point + ((hit.point - _aimOrigin).normalized) * _hitDetectionMultiplier;
 
             // Invoke the unity event
             if (shootEvent != null)
             {
-                shootEvent.Invoke(adjustedHitPoint, transform.position);
+                shootEvent.Invoke(adjustedHitPoint, _aimOrigin);
             }
         }
     }
+
+    #region Public Event Handlers
+
+    public void ChangeTargetLaserOrigin(Vector2 newOrigin)
+    {
+        _aimOrigin = newOrigin;
+    }
+
+    #endregion
 }
